705b2f55d85e38b79ccd9910bc63cdf6c5312c91
Mcjayoap <drdonjoe@gmail.com>
2022-10-19 15:10:25 -0700

    .

--- main.h
+++ main.h
@@ -1,43 +1,47 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
 
-void print_buffer(char buffer[], int *buff_ind);
-int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_decimal(va_list arg);
-int print_binary(va_list arg);
-int print_octal(va_list arg);
-int print_hexa_lower(va_list arg);
-int print_hexa_upper(va_list arg);
-int print_unsigned(va_list arg);
-int print_str_unprintable(va_list arg);
-int print_str_reverse(va_list arg);
-int print_ptr(va_list arg);
-int print_rot13(va_list arg);
-int print_percent(va_list arg __attribute__((unused)));
-int print_number(int n);
-int print_unsigned_number(unsigned int n);
-int _putchar(char c);
-int _puts(char *str, int ascii);
-int _nbr_len(int prmNumber);
-int _strlen_recursion(char *s);
-int convert_alpha_numeric(int nb, int upper);
-char *convert_rot13(char *str);
-char *convert_base(unsigned long nb, unsigned int base, int upper);
-char *_strdup(char *str);
-char *convert_base_pointer(unsigned long p);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
 
 /**
- * struct flags_printf - struct conversion to function
- * @c: flag string
- * @f: pointer to func
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
  */
 
-typedef struct flags_printf
+typedef struct format
 {
-	char *c;
-	int (*f)(va_list);
-} flags_p;
+	char *id;
+	int (*f)();
+} convert_match;
+
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
+int _printf(const char *format, ...);
+
 #endif
